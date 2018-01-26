1e90364087799100a40c85acb5be1855ce931d43
Sayantan Mukherjee <sayantanmukherjee3024@gmail.com>
2018-01-26 17:30:01 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version: ruby 2.3.1p112 (2016-04-26) [x86_64-linux-gnu]
+
+* Database: mysql
+
+* Rails version: Rails 5.1.4
+
+* Description: 	A simple project on post creation. Only an user who is loged in can do 
+				CRUD operations on the posts. An user can perform Edit and Destroy operations only his/her post. He/She can only view others post.
+
+						This project is a demo for how to use Devise gem.
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,63 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! ,except: [:index]
+  
+  def index
+    # binding.pry
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def edit
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: t('post_create_success') }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: t('post_update_success')}
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: t('post_destroy_success') }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
